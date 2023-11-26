b66a0d3aafe4ee8d972169c75ea44e17228fbd75
Telladi <Telladirtv@mail.ru>
2023-11-26 19:41:47 +0300

    1-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [tasks, setTasks]=useState<TaskType[]>( [{ id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }])
+    return <div className="App">
+        <Todolist title={"title"} tasks={tasks}/>
     </div>
-  );
+
 }
 
 export default App;
